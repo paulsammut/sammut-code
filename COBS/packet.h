9c4ad42c85cf5f6ca74b943ce61f62b4a38cc932
paulsammut <paul.c.sammut@gmail.com>
2016-11-23 11:53:22 -0500

    fixed the symbolic link

--- COBS/packet.h
+++ COBS/packet.h
@@ -1,1 +1,11 @@
-../packet.h
+#pragma once
+
+// Here we decode packets. We also encode packets. 
+// We also check for data on the serial bus. 
+// We use COBS as defined in this paper
+// http://www.stuartcheshire.org/papers/cobsforton.pdf
+
+
+void StuffData(const unsigned char *ptr, unsigned long length, unsigned char *dst);
+
+void UnStuffData(const unsigned char *ptr, unsigned long length, unsigned char *dst);
